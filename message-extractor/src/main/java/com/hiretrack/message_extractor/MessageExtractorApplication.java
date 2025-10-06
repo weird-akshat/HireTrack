f55e85125d4b9ce17971e92728f56f5d8e4f3e11
Akshat Pandey <thatweirdakshat@gmail.com>
2025-10-06 11:39:53 +0530

    add new message extractor service

---
+++ message-extractor/src/main/java/com/hiretrack/message_extractor/MessageExtractorApplication.java
@@ -0,0 +1,13 @@
+package com.hiretrack.message_extractor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageExtractorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageExtractorApplication.class, args);
+	}
+
+}
---
+++ message-extractor/src/test/java/com/hiretrack/message_extractor/MessageExtractorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hiretrack.message_extractor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageExtractorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
