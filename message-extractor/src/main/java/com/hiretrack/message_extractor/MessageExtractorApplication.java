191bc277262183d4af5ec6cc477a67e172e835fc
Akshat Pandey <thatweirdakshat@gmail.com>
2025-10-07 10:08:42 +0530

    Connected message-extractor to eureka server

--- message-extractor/src/main/java/com/hiretrack/message_extractor/MessageExtractorApplication.java
+++ message-extractor/src/main/java/com/hiretrack/message_extractor/MessageExtractorApplication.java
@@ -2,8 +2,11 @@ package com.hiretrack.message_extractor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
+
 public class MessageExtractorApplication {
 
 	public static void main(String[] args) {
