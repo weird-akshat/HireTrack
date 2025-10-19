1e68cc375306b36de82562b94e206a0c5ca5b52b
Akshat Pandey <thatweirdakshat@gmail.com>
2025-10-19 16:05:55 +0530

    created entity manager service

---
+++ entity-manager/src/main/java/com/hiretrack/entity_manager/EntityManagerApplication.java
@@ -0,0 +1,13 @@
+package com.hiretrack.entity_manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntityManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntityManagerApplication.class, args);
+	}
+
+}
---
+++ entity-manager/src/test/java/com/hiretrack/entity_manager/EntityManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hiretrack.entity_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntityManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
