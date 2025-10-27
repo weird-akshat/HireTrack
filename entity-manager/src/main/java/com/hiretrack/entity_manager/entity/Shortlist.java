6baff9a4564af142565143a4b19566542382a50e
Akshat Pandey <thatweirdakshat@gmail.com>
2025-10-28 01:13:17 +0530

    constructor

--- entity-manager/src/main/java/com/hiretrack/entity_manager/entity/Shortlist.java
+++ entity-manager/src/main/java/com/hiretrack/entity_manager/entity/Shortlist.java
@@ -2,14 +2,18 @@ package com.hiretrack.entity_manager.entity;
 
 import com.hiretrack.entity_manager.dto.StudentDto;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Entity
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Shortlist {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
