f7b63bc38371461895d67570b8dc17c7e636f1b9
Akshat Pandey <thatweirdakshat@gmail.com>
2025-10-27 17:30:07 +0530

    deployment

--- job-management/src/test/java/com/hiretrack/job_management/JobManagementApplicationTests.java
+++ job-management/src/test/java/com/hiretrack/job_management/JobManagementApplicationTests.java
@@ -1,9 +1,13 @@
 package com.hiretrack.job_management;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
+//@ImportAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+
 class JobManagementApplicationTests {
 
 	@Test
